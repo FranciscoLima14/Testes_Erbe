70becd85b09c0777990b9528572189bde541f507
FranciscoLima14 <francisco.lima@polijunior.com.br>
2026-03-09 21:52:28 -0300

    MUDANÇA

--- painel.py
+++ painel.py
@@ -103,7 +103,7 @@ def carregar_bases():
     verificar_arquivo("SETTLED.xlsx")
     verificar_arquivo("relatorio_tratado.xlsx")
 
-    entradas = pd.read_excel("ENTRADAS_ANALISE.xlsx")
+    entradas = pd.read_excel("PLAN.xlsx")
     settled = pd.read_excel("SETTLED.xlsx")
     relatorio = pd.read_excel("relatorio_tratado.xlsx")
 
